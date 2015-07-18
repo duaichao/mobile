40443cd7985c022244078047964f7747931ae331
duaichao <ayduaichao@gmail.com>
2015-07-18 16:47:09 +0800

    11

--- webapp/app/controller/Index.js
+++ webapp/app/controller/Index.js
@@ -2,7 +2,7 @@ Ext.define('app.controller.Index', {
     extend: 'Ext.app.Controller',
     config: {
         control: {
-        	'homeContainer button[iconCls=icon-geren]':{
+        	'home button[iconCls=icon-geren]':{
         		tap : function(btn){
         			util.war('loading...');
         		}
