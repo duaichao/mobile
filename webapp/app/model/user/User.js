1dcff608ca81ac1d85a24ac2095339b6aad5948f
duaichao <ayduaichao@gmail.com>
2015-07-18 18:07:39 +0800

    1

--- webapp/app/model/user/User.js
+++ webapp/app/model/user/User.js
@@ -7,40 +7,25 @@ Ext.define('app.model.user.User', {
             type: 'int'
         },
         {
-            name: 'userPurviewId',
-            type: 'string'
-        },
-        {
-            name: 'userPurview',//用户类型 分销商 供应商 站长 集团站长 代理社站长 admin
-            type: 'string'
-        },
-        {
-        	name:'retailType',//是否是代理社 siteGroup,siteProxy
-        	type:'string'
-        },
-        {
-            name: 'isSub',//是否是子用户
-            type: 'string'
-        },
-        {
-            name: 'msgCount',//消息数量
-            type: 'string'
-        },
-        {
-            name: 'name',
+            name: 'username',
             type: 'string'
         },
         {
             name: 'password',
             type: 'string'
-        }],
+        },
+        {
+        	name:'email',
+        	type:'string'
+        },
+        ],
         validations: [
         {
             field: 'password',
             type: 'presence',
             message: '请输入密码!'
         }, {
-            field: 'name',
+            field: 'username',
             type: 'presence',
             message: '请输入用户名!'
         }],
