018edcd0c7f199fa704d5c2dae01653af63addfe
duaichao <ayduaichao@gmail.com>
2015-07-19 17:37:07 +0800

    1

---
+++ webapp/app/model/Course.js
@@ -0,0 +1,27 @@
+Ext.define('app.model.Course', {
+    extend: 'Ext.data.Model',
+    config: {
+        fields: [{
+            name: 'course_id',
+            type: 'string'
+        },{
+        	name:'course_name',
+        	type:'string'
+        },{
+        	name:'total_num',
+        	type:'string'
+        },{
+        	name:'process_num',
+        	type:'string'
+        },{
+        	name:'passing_percent',
+        	type:'string'
+        },{
+        	name:'average_speed',
+        	type:'string'
+        },{
+        	name:'correct_percent',
+        	type:'string'
+        }]
+    }
+});
