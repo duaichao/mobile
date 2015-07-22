c0821f1c920df9ba9833a677454039a9d9bd84c5
duaichao <ayduaichao@gmail.com>
2015-07-22 23:06:36 +0800

    d

---
+++ webapp/app/controller/Exercise.js
@@ -0,0 +1,45 @@
+Ext.define('app.controller.Exercise', {
+    extend: 'Ext.app.Controller',
+    config: {
+        refs: {
+        	exerciseView:'exerciseview'
+        },
+        control: {
+        	'exerciseview button#prev':{
+        		tap:'doPage'
+        	},'exerciseview button#next':{
+        		tap:'doPage'
+        	}
+        }
+    },
+    doPage :function(btn){
+    	var p=this.getExerciseView(),
+		st = p.down('exerciselist').getStore(),
+		pbtn = p.down('button#prev'),
+		nbtn = p.down('button#next'),
+		total = st.getTotalCount(),pageSize = st.getPageSize(),
+		pages = total/pageSize;
+    	
+    	if(st.getTotalCount()%st.getPageSize()>0){pages++;}
+    	st.getProxy().setExtraParams(Ext.applyIf(p.getDefaultParams(),st.getProxy().getExtraParams()));
+    	
+    	if(pbtn.getItemId()==btn.getItemId()){
+    		if(st.currentPage-1==1){
+    			pbtn.disable();
+    		}
+    		if(st.currentPage-1<pages&&nbtn.isDisabled()){
+    			nbtn.enable();
+    		}
+    		st.previousPage();
+    	}else{
+    		if(st.currentPage+1==pages){
+				nbtn.disable();
+			}
+    		if(st.currentPage+1>1&&pbtn.isDisabled()){
+    			pbtn.enable();
+    		}
+    		st.nextPage();
+    	}
+		
+    }
+});
